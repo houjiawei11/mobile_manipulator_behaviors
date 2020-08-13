6626003ce41edb2bfe1eb967c2d021f96c3c6e73
Hou Jiawei <houjw@shanghaitech.edu.cn>
2020-08-14 00:45:17 +0800

    add head controller actionlib

--- mobile_manipulator_flexbe_behaviors/src/mobile_manipulator_flexbe_behaviors/bring_something_from_a_to_b_sm.py
+++ mobile_manipulator_flexbe_behaviors/src/mobile_manipulator_flexbe_behaviors/bring_something_from_a_to_b_sm.py
@@ -8,14 +8,16 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from flexbe_navigation_states.move_base_state import MoveBaseState
+from mobile_manipulator_flexbe_states.head_actionlib_state import HeadActionState
 from mobile_manipulator_flexbe_states.detect_obj_srv_state import DetectObjState
+from flexbe_navigation_states.move_base_state import MoveBaseState
 from mobile_manipulator_flexbe_states.pick_srv_state import PickState
 from mobile_manipulator_flexbe_states.detect_plane_srv_state import DetectPlaneState
 from mobile_manipulator_flexbe_states.place_srv_state import PlaceState
+from flexbe_manipulation_states.moveit_to_joints_state import MoveitToJointsState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
-from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Pose2D, Point
 # [/MANUAL_IMPORT]
 
 
@@ -49,8 +51,12 @@ class bring_something_from_a_to_bSM(Behavior):
 	def create(self):
 		# x:26 y:401, x:564 y:234
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
-		_state_machine.userdata.pick_waypoint = Pose2D(3, -1,0)
-		_state_machine.userdata.place_waypoint = Pose2D(5, 1,0)
+		_state_machine.userdata.pick_waypoint = Pose2D(2.6, 3.0,0)
+		_state_machine.userdata.place_waypoint = Pose2D(-4, 3.8, 1.57)
+		_state_machine.userdata.picking_state = [0.385, 0.476, 1.266, -0.722, 1.954, -1.708, 2.0197, -1.415]
+		_state_machine.userdata.prepare_state = [0.337, 1.334, 1.328, -0.145, 1.811, 0.0, 1.639, 0.042]
+		_state_machine.userdata.start_point = Point(0.48, 0.0, 0.5)
+		_state_machine.userdata.look_up_point = Point(2.0, 0.0, 1.1)
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -59,14 +65,14 @@ class bring_something_from_a_to_bSM(Behavior):
 
 
 		with _state_machine:
-			# x:188 y:56
-			OperatableStateMachine.add('nav_pick',
-										MoveBaseState(),
-										transitions={'arrived': 'detect_obj', 'failed': 'failed'},
-										autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
-										remapping={'waypoint': 'pick_waypoint'})
+			# x:35 y:57
+			OperatableStateMachine.add('look_down',
+										HeadActionState(),
+										transitions={'head_arrived': 'nav_pick', 'command_error': 'failed'},
+										autonomy={'head_arrived': Autonomy.Off, 'command_error': Autonomy.Off},
+										remapping={'point2see': 'start_point'})
 
-			# x:491 y:56
+			# x:740 y:48
 			OperatableStateMachine.add('detect_obj',
 										DetectObjState(),
 										transitions={'continue': 'pick_obj', 'failed': 'failed'},
@@ -79,13 +85,13 @@ class bring_something_from_a_to_bSM(Behavior):
 										autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'waypoint': 'place_waypoint'})
 
-			# x:821 y:52
+			# x:1056 y:45
 			OperatableStateMachine.add('pick_obj',
 										PickState(),
-										transitions={'continue': 'nav_place', 'failed': 'failed'},
+										transitions={'continue': 'pick_moving_state', 'failed': 'pick_moving_state'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:503 y:396
+			# x:493 y:400
 			OperatableStateMachine.add('detect_plane',
 										DetectPlaneState(),
 										transitions={'continue': 'place_obj', 'failed': 'failed'},
@@ -96,6 +102,34 @@ class bring_something_from_a_to_bSM(Behavior):
 										PlaceState(),
 										transitions={'continue': 'finished', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
+
+			# x:1074 y:214
+			OperatableStateMachine.add('pick_moving_state',
+										MoveitToJointsState(move_group='arm_with_torso', joint_names=['torso_lift_joint', 'shoulder_pan_joint', 'shoulder_lift_joint', 'upperarm_roll_joint', 'elbow_flex_joint', 'forearm_roll_joint', 'wrist_flex_joint', 'wrist_roll_joint'], action_topic='/move_group'),
+										transitions={'reached': 'look_up', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'joint_config': 'picking_state'})
+
+			# x:499 y:47
+			OperatableStateMachine.add('prepare_state',
+										MoveitToJointsState(move_group='arm_with_torso', joint_names=['torso_lift_joint', 'shoulder_pan_joint', 'shoulder_lift_joint', 'upperarm_roll_joint', 'elbow_flex_joint', 'forearm_roll_joint', 'wrist_flex_joint', 'wrist_roll_joint'], action_topic='/move_group'),
+										transitions={'reached': 'detect_obj', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'joint_config': 'prepare_state'})
+
+			# x:260 y:56
+			OperatableStateMachine.add('nav_pick',
+										MoveBaseState(),
+										transitions={'arrived': 'prepare_state', 'failed': 'failed'},
+										autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'waypoint': 'pick_waypoint'})
+
+			# x:1069 y:368
+			OperatableStateMachine.add('look_up',
+										HeadActionState(),
+										transitions={'head_arrived': 'nav_place', 'command_error': 'failed'},
+										autonomy={'head_arrived': Autonomy.Off, 'command_error': Autonomy.Off},
+										remapping={'point2see': 'look_up_point'})
 
 
 		return _state_machine
