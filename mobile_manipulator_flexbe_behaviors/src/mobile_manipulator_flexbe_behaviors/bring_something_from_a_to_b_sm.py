5e3691a69a88a1ada1e9d9e945e999b7c66f4e7d
yizheng <zhangyizheng1996@outlook.com>
2020-08-22 23:13:26 +0800

    update param for fetch, which works

--- mobile_manipulator_flexbe_behaviors/src/mobile_manipulator_flexbe_behaviors/bring_something_from_a_to_b_sm.py
+++ mobile_manipulator_flexbe_behaviors/src/mobile_manipulator_flexbe_behaviors/bring_something_from_a_to_b_sm.py
@@ -50,8 +50,8 @@ class bring_something_from_a_to_bSM(Behavior):
 	def create(self):
 		# x:26 y:401, x:564 y:234
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
-		_state_machine.userdata.pick_waypoint = Pose2D(2.6, 3.0,0)
-		_state_machine.userdata.place_waypoint = Pose2D(-3, 3.8, 1.57)
+		_state_machine.userdata.pick_waypoint = Pose2D(-0.8, -3.65, -1.57)
+		_state_machine.userdata.place_waypoint = Pose2D(-8.0, 4.13, 3.14)
 		_state_machine.userdata.picking_state = [0.385, 0.476, 1.266, -0.722, 1.954, -1.708, 2.0197, -1.415]
 		_state_machine.userdata.prepare_state = [0.337, 1.334, 1.328, -0.145, 1.811, 0.0, 1.639, 0.042]
 
@@ -94,12 +94,6 @@ class bring_something_from_a_to_bSM(Behavior):
 										transitions={'continue': 'place_obj', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:193 y:392
-			OperatableStateMachine.add('place_obj',
-										PlaceState(),
-										transitions={'continue': 'finished', 'failed': 'failed'},
-										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
-
 			# x:1074 y:214
 			OperatableStateMachine.add('pick_moving_state',
 										MoveitToJointsState(move_group='arm_with_torso', joint_names=['torso_lift_joint', 'shoulder_pan_joint', 'shoulder_lift_joint', 'upperarm_roll_joint', 'elbow_flex_joint', 'forearm_roll_joint', 'wrist_flex_joint', 'wrist_roll_joint'], action_topic='/move_group'),
@@ -114,6 +108,45 @@ class bring_something_from_a_to_bSM(Behavior):
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
 										remapping={'joint_config': 'prepare_state'})
 
+			# x:178 y:174
+			OperatableStateMachine.add('nav_pick',
+										MoveBaseState(),
+										transitions={'arrived': 'prepare_state', 'failed': 'failed'},
+										autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'waypoint': 'pick_waypoint'})
+
+			# x:1053 y:513
+			OperatableStateMachine.add('look_up',
+										HeadActionState(),
+										transitions={'head_arrived': 'nav_place', 'command_error': 'failed'},
+										autonomy={'head_arrived': Autonomy.Off, 'command_error': Autonomy.Off},
+										remapping={'point2see': 'look_up_point'})
+
+			# x:598 y:600
+			OperatableStateMachine.add('find_apriltag',
+										HeadActionState(),
+										transitions={'head_arrived': 'detect_plane', 'command_error': 'failed'},
+										autonomy={'head_arrived': Autonomy.Off, 'command_error': Autonomy.Off},
+										remapping={'point2see': 'see_apriltag'})
+
+			# x:130 y:479
+			OperatableStateMachine.add('place_obj',
+										PlaceState(),
+										transitions={'continue': 'finished_pose', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
+
+			# x:1056 y:45
+			OperatableStateMachine.add('pick_obj',
+										PickState(),
+										transitions={'continue': 'pick_moving_state', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
+
+			# x:92 y:342
+			OperatableStateMachine.add('finished_pose',
+										MoveitToJointsState(move_group='arm_with_torso', joint_names=['torso_lift_joint', 'shoulder_pan_joint', 'shoulder_lift_joint', 'upperarm_roll_joint', 'elbow_flex_joint', 'forearm_roll_joint', 'wrist_flex_joint', 'wrist_roll_joint'], action_topic='/move_group'),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'joint_config': 'picking_state'})
 
 		return _state_machine
 
